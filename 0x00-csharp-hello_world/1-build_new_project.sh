2cdbecc84ba8448ed0a8012974700369643b7e53
Andres Felipe Escobar Cardona <57599404+AndresEscobarDev@users.noreply.github.com>
2021-11-29 17:37:04 -0500

    fixed task 0,1,2,3

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Initializes and builds a new C# project inside a folder titled 1-new_project
-dotnet new console -o ./1-new_project
+dotnet new console --output ./1-new_project
 dotnet build ./1-new_project
