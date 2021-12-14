41ce86e356acdc176a248fa31813d1e4ecc855e8
Andres Felipe Escobar Cardona <57599404+AndresEscobarDev@users.noreply.github.com>
2021-12-13 19:04:04 -0500

    0x04. C# - Exceptions All tasks

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+  public static int SafePrint(List<int> myList, int n)
+  {
+    int count = 0;
+    try
+    {
+      for (; count < n; count++)
+      {
+        Console.WriteLine(myList[count]);
+      }
+    }
+    catch (Exception)
+    {
+      return count;
+    }
+    return count;
+  }
+}
